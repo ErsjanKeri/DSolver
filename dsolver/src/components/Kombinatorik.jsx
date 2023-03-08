c3817feb2a6d23d2ca21519382ae17d87af70174
Juli436 <juliafranzik@gmail.com>
2023-03-08 12:18:24 +0100

    finished, maybe some design neccessary

--- dsolver/src/components/Kombinatorik.jsx
+++ dsolver/src/components/Kombinatorik.jsx
@@ -1,15 +1,9 @@
 import React, { useState, useEffect, Fragment } from 'react'
-import  { Button, message, Row, Col, Input, Select, Upload, InputNumber, Typography, Dropdown, Space, Menu } from "antd"
-import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
-import { Navigate } from "react-router-dom";
+import  { Button,  Row, Col, Input, Select, Upload, InputNumber, Typography, Dropdown, Space, Menu } from "antd"
 
 
 
 
-
-
-
-const { Option } = Select;
 const { Title, Text } = Typography;
 
 
@@ -19,103 +13,52 @@ function Kombinatorik() {
     const [calculated, setCalculated] =useState(false);
     const [result, setResult] = useState(0);
 
+    const [calcN, setCalcN]= useState(0);
+    const [calcK, setCalcK]= useState(0);
+    const [calcKind, setCalcKind] = useState("")
+
     
   const [kInput, setKInput] = useState("")
   const [nInput , setNInput] = useState("")
-  const kof = [{ 
-    key: '1',
-    label: (
-      <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
-        1st menu item
-      </a>
-    ),
-  },
-  {
-    key: '2',
-    label: (
-      <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
-        2nd menu item (disabled)
-      </a>
-    ),
-    
-    disabled: true,
-  },
-  {
-    key: '3',
-    label: (
-      <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
-        3rd menu item (disabled)
-      </a>
-    ),
-    disabled: true,
-  },
- ]
+  
 
-  const [paragraphs, setParagraphs] = useState([{title : "", content : ""}])
-    // done 
-  const [thumbnail, setThumbnail] = useState(null)
-  const [preview, setPreview] = useState(null)
-
-  const [loading, setLoading] = useState(false)
-
-  const [selectedTags, selectTag ] = useState([])
-  const [selectedCat, selectCat] = useState("")
-
-  const [postId, setPostId] = useState(null)
+  
 
   useEffect(() => {
 
   }, [])
 
-  const addNewParagraph = () => {
-      setParagraphs([...paragraphs, {title : null, content : null}])
-  }
-
-  const handleParagraphChange = (i, e) => {
-      const newParagraphs = [...paragraphs]
-      newParagraphs[i][e.target.name] = e.target.value 
-      setParagraphs(newParagraphs)
-  }
-
-  const removeParagraph = (i) => {
-    const newParagraphs = [...paragraphs]
-    newParagraphs.splice(i, 1)
-    setParagraphs(newParagraphs)
-  }
-
-  function getBase64(img, callback) {
-    const reader = new FileReader();
-    console.log(img)
-    reader.addEventListener('load', () => callback(reader.result));
-    reader.readAsDataURL(img);
-  }
-
-  function beforeUpload(file) {
-    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-    if (!isJpgOrPng) {
-      message.error('You can only upload JPG/PNG file!');
-    } else {
-      setThumbnail(file)
-    }
-    
-    return isJpgOrPng;
-  }
+  
+  
  
 
   function calculate(){
-    setCalculated(true);
+    
     console.log('abc')
-    if(nInput === "" || kInput === ""){
+    if(nInput === "" || kInput === "" || kind ===""){
         console.log("keine nummern eingegeben")
       }
       else{
     
         if(kind === 's1'){
             setResult(calcs1(nInput, kInput));
+            setCalcKind('s');
 
         }
+        else if(kind === 's2' ){
+          setResult(calcs2(nInput, kInput));
+          setCalcKind('S');
 
-      
+        }
+        else if(kind === 'p'){
+          setResult(calcp(nInput, kInput));
+          setCalcKind('P');
+
+        }
+        
+        setCalcK(kInput)
+        setCalcN(nInput)
+        setCalculated(true);
         
       }
   }
@@ -123,11 +66,34 @@ function Kombinatorik() {
     if(n===k){
         return 1;
     }
-    if(n===0){
+    if(n===0|k===0){
         return 0;
     }
     return calcs1(n-1, k-1) + calcs1(n-1,k)*(n-1)
 
+  }
+
+  function calcs2(n, k){
+    if(n===k){
+      return 1;
+    }
+    if(n===0|k===0){
+      return 0;
+  }
+ return calcs2(n-1, k-1)+ calcs2(n-1, k)*k;
+  }
+
+  function calcp(n, k){
+    if(n===k){
+      return 1;
+    }
+    if(k>n){
+      return 0;
+    }
+    if(k===0){
+      return 0;
+    }
+    return calcp(n-1,k-1)+calcp(n-k, k);
   }
 
 
@@ -141,11 +107,11 @@ function Kombinatorik() {
 
     <Col xs={24} >
         <Title style={{"textAlign":"center"}} level={4}>Kombinatorik Calculator</Title>
+        
         <Row justify={"center"}>
-          <Col xs={12}>
+          <Col xs={7}>
             <form className='input_group mb-2' >
-            
-            <div className='input_number'>
+              
             <Select
                         onChange={(e) => { setKind(e) }}
                         options={[
@@ -157,10 +123,26 @@ function Kombinatorik() {
                         placeholder={"Auswählen..."}
                     />
               
+              
+            
+            
+          </form>
+          </Col>
+        </Row>
+        <Row justify={"center"}>
+          <Col xs={12}>
+            <form className='input_group mb-2' >
+            
+            
+            <div className='input_number'>
+           
+              
               <InputNumber value={nInput} onChange={(value) => {setNInput(value)}} placeholder='n'/>
               <InputNumber value={kInput} onChange={(value) => {setKInput(value)}} placeholder='k'/>
-              <Button className="submit-btn" type="primary" onClick={calculate} disabled={(nInput === "" || kInput === "")}>Calculate</Button>
+              <Button className="submit-btn" type="primary" onClick={calculate} disabled={(nInput === "" || kInput === "" ||kind === "")}>Calculate</Button>
+            
             </div>
+            
             
           </form>
           </Col>
@@ -170,9 +152,9 @@ function Kombinatorik() {
 </Row> 
 {calculated ? (
           <Row gutter={[0, 8]} justify={"center"} className="mb-4">
-            <Col xs={14}>
-                  <Button>{kind}: {result}</Button>
-              </Col>
+           
+                  <Title style={{"textAlign":"center"}} level ={3}>{calcKind}<sub>{calcN},{calcK}</sub> = {result}</Title>
+             
            
           </Row>
      
