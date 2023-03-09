b4903cae9e56ddc2a9400074c456bf3c8c79a54f
Ersjan Këri <ge87cin@mytum.de>
2023-03-09 11:42:44 +0100

    ch

--- dsolver/src/components/Mainpage.jsx
+++ dsolver/src/components/Mainpage.jsx
@@ -1,8 +1,18 @@
+import { Col, Row } from 'antd'
 import React from 'react'
 
 function Mainpage() {
+  const arrs = [1,2,3,4,5,6]
   return (
-    <div>Mainpage</div>
+    <Row gutter={[24,24]}>
+        {arrs.map(i => {
+            return (
+            <Col className='gutter-row' xs={8} style={{"height" : "200px"}}>
+                {i} 
+            </Col>
+            )
+        })}
+    </Row>
   )
 }
 
